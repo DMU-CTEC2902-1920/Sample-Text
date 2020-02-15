d0dbd340283cb48b4c7e7c563dc6b3f3aa9ac2e6
LanternCode <Adam.Machowczyk@gmail.com>
2020-02-15 20:45:55 +0000

    Organized file structure. Added and removed some empty spaces and line breakes.

--- dmuBlogger/dmuBlogger/Properties/AssemblyInfo.cs
+++ dmuBlogger/dmuBlogger/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("dmuBlogger")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright © Team SampleText  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
