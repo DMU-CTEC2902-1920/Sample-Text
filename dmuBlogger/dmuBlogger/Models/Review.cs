c33a3c41f884cfb5eeef70ab1f4da957d99f5297
Benjamin Jones <P2423891@my365.dmu.ac.uk>
2020-03-11 12:26:38 +0000

    Added add/edit/delete/view for reviews and developers

---
+++ dmuBlogger/dmuBlogger/Models/Review.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace dmuBlogger.Models
+{
+    public class Review
+    {
+        public virtual int ReviewID { get; set; }
+        public virtual int GenreId { get; set; }
+        public virtual string Title { get; set; }
+        public virtual string Description { get; set; }
+        public virtual string ImageURL { get; set; }
+        public virtual Developer Developer { get; set; }
+    }
+}
