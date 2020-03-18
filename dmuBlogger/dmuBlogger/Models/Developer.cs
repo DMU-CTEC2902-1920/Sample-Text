688c725eb983131a14db16b2ebebfd395bfcc4bd
LanternCode <Adam.Machowczyk@gmail.com>
2020-03-18 12:18:21 +0000

    Scaffold game, put a dropdown list, prepare for filling the list

---
+++ dmuBlogger/dmuBlogger/Models/Developer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace dmuBlogger.Models
+{
+    public class Developer
+    {
+        public virtual int DeveloperId { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string Description { get; set; }
+    }
+}
---
+++ dmuBlogger/dmuBlogger/Models/Game.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace dmuBlogger.Models
+{
+    public class Game
+    {
+        [Key]
+        public Int32 GameId { get; set; }
+        [Required]
+        public String GameName { get; set; }
+        [Required]
+        public String GameReleaseDate { get; set; }
+
+        public List<Developer> GameDevelopers { get; set; }
+    }
+}
